734d350b2d7ad198c7d0f94fef99973282505b94
Tony Medina <cayapadevelop@gmail.com>
2025-05-20 15:40:02 +0200

    creacion endpoint tipo POST addEmpleado

--- src/main/java/com/example/demo3/DemoRestController.java
+++ src/main/java/com/example/demo3/DemoRestController.java
@@ -9,6 +9,8 @@ import java.util.ArrayList;
 import java.util.List;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -17,24 +19,36 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 public class DemoRestController {
-
-    private static final List<Empleado> empleados = new ArrayList<>(); 
-    static{
-            empleados.add(new Empleado( 1, "Tony", "Medina", 35 ));
-            empleados.add(new Empleado(2, "Dylan", "Piña",5 ));
-            empleados.add(new Empleado(3, "Angelberth", "Noguera", 32 ));
-}
+    
+    private static final List<Empleado> empleados = new ArrayList<>();    
+    
+    static {
+        empleados.add(new Empleado(1, "Tony", "Medina", 35));
+        empleados.add(new Empleado(2, "Dylan", "Piña", 5));
+        empleados.add(new Empleado(3, "Angelberth", "Noguera", 32));
+    }
     
     @GetMapping("/")
     public String holaMundo() {
         return "¡Hola mundo!";
     }
+    
     @GetMapping("/empleados")
-    public List<Empleado>  empleados(){
+    public List<Empleado> empleados() {
         return empleados;
     }
+    
     @GetMapping("/empleados/{id}")
-    public Empleado getIdEmpleado(@PathVariable Long id){
-    return empleados.stream().filter(empleado -> empleado.getId() == id).findFirst().orElse(null);
+    public Empleado getIdEmpleado(@PathVariable Long id) {
+        return empleados.stream().filter(empleado -> empleado.getId() == id).findFirst().orElse(null);
     }
+    
+    @PostMapping("/empleados")
+    public String addEmpleado(@RequestBody Empleado elEmpleado) {
+        Long nuevoId = empleados.stream().mapToLong(Empleado::getId).max().orElse(0) + 1;
+        elEmpleado.setId(nuevoId);
+        empleados.add(elEmpleado);
+        return "El empleado se ha añadido correctamente";
+    }
+    
 }
